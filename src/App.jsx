1b997594b2e77055de17407c9cae41cdd2d59e84
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-02-11 21:57:05 -0600

    feat: It was implemented the React Router Library
    Installation of React Router Library an it was implemented first simple routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+import { Route, Routes } from "react-router";
+
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
+
 import { Search } from "./pages/Search.jsx";
 import { Home } from "./pages/Home.jsx";
 import { NotFound } from "./pages/NotFound.jsx";
 import { Contact } from "./pages/Contact.jsx";
-import { Route } from "./components/Route.jsx";
 
 
 function App() {
   return (
     <>
       <Header />
-        <Route path="/" component={Home} />
-        <Route path="/search" component={Search} />
-        <Route path="/contact" component={Contact} />
-        <Route path="*" component={NotFound} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   )
