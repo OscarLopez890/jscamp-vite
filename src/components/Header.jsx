f8c146731b7f28ea6a90a578610026403bf0b08d
OscarLopez890 <oscarcastillolopez890@gmail.com>
2026-02-21 21:11:19 -0600

    feat: Implemented NavLink in Header
    Implemented NavLink to principal navigate on header, and styles for active page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import styles from "./Header.module.css"
 import { Link } from "./Link.jsx"
+import { NavLink } from "react-router"
 
 export function Header(){
   return (
     <header className={styles.header}>
-      <Link href="/" style={{textDecoration: 'none'}}>
+      <NavLink to="/" style={{textDecoration: 'none'}}>
         <h1 style={{color: 'white'}}>
           <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-brackets-angle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 4l-5 8l5 8" /><path d="M16 4l5 8l-5 8" /></svg>
           DevJobs
         </h1>
-      </Link>
+      </NavLink>
       <nav>
-          <Link href="/" target="_self" rel="noopener noreferrer">Inicio</Link>
-          <Link href="/search">Empleos</Link>
-          <Link href="/contact">Contacto</Link>
+          <NavLink className={({isActive}) => isActive ? styles.navLinkActive : ''}  to="/" end>Inicio</NavLink>
+          <NavLink className={({isActive}) => isActive ? styles.navLinkActive : ''}  to="/search">Empleos</NavLink>
+          <NavLink className={({isActive}) => isActive ? styles.navLinkActive : ''}  to="/contact">Contacto</NavLink>
       </nav>
       <div>
           <a href="register.html">Publicar un empleo</a>
